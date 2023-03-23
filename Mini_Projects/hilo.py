a48d1354d23365f69179ff14e4e8101115b973b7
Steffan Lynch <hello@steffanlynch.com>
2023-03-23 17:44:05 +0000

    Added comments and documentation

--- Mini_Projects/hilo.py
+++ Mini_Projects/hilo.py
@@ -8,10 +8,13 @@ guesses = 1
 while low != high:
     print("\tGuessing in the range of {} and {}".format(low, high))
     guess = low + (high - low) // 2
+    # basically, it guesses the midpoint between the range. when you say higher or lower, the range changes.
     hi_lo = input("My guess is {}. Should I guess higher or lower? "
                   "Enter h if higher, l if lower or c if correct"
 
                   .format(guess)).casefold()
+                    # this format method basically inserts those variables in the order they appear, rather than having to concatenate them seperately.
+                    # it makes things easier and more dynamic
     if hi_lo == "h":
         # Guess higher. So the lowest value now becomes 1 higher than the guess
         low = guess + 1
@@ -21,6 +24,10 @@ while low != high:
     elif hi_lo == "c":
         print("I got it in {} guesses".format(guesses))
         break
+        # break is a keyword in python
+        # this break means that it cuts/exits out of the loop. As its got the answer correct, there is no need to go through the loop again.
+        # used particular in while loops as they have the potential to run infinitely and then crash the program
+        # ^^^^ for example, while 1 = 1 or while True
     else:
         print("Print only h, l or c")
 
@@ -30,5 +37,4 @@ else:
     print("You thought of the number {}".format(low))
     print("I guessed your number in {} guesses".format(guesses))
 
-    #this only works when the numbers (high and low) converge to the
-    #number
+    #this only works when the numbers (high and low) converge to the number
